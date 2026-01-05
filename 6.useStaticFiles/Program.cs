ae74e4db27bf3fd3780e6accfeefefb5d580b9db
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-01-05 23:35:33 +0530

    using multiple folders for static files

--- 6.useStaticFiles/Program.cs
+++ 6.useStaticFiles/Program.cs
@@ -1,6 +1,20 @@
-// create a folder named wwwroot
+// wwwroot
+// create a folder named wwwroot put the files or img in it 
+// it can be access directly by typing the url http://localhost:5000/filename.pdf
+// also need to add app.UseStaticFiles();
 
-var builder = WebApplication.CreateBuilder(args);
+
+// if we are using other foler name instead of wwwroot we have to specify it in the code like below
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+var builder = WebApplication.CreateBuilder(
+    new WebApplicationOptions()
+    {
+        WebRootPath = "myroot"
+    });
 var app = builder.Build();
 
 app.UseStaticFiles();
