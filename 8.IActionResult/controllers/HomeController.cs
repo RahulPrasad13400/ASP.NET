75dedce8bfbd182e7623827a69648147367ef653
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-01-09 00:51:06 +0530

    Files and IActionResults

---
+++ 8.IActionResult/controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultcontrollers
+{
+    public class HomeController : Controller
+    {
+        [Route("abc")]
+        public IActionResult Index()
+        {
+            //return Content("Hello World");
+
+            //Book Id can't be empty 
+            if (string.IsNullOrWhiteSpace(Convert.ToString(Request.Query["bookid"])))
+            {
+                return Content("Book Id can't be null or empty");
+            }
+
+            //Book Id should be between 1 and 1000
+            int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
+            if(bookId <= 0)
+            {
+                return Content("Book id can't be less than or equal to zero");
+            }
+            if(bookId > 1000)
+            {
+                Response.StatusCode = 400;
+                return Content("Book id can't be greater than 1000");
+            }
+
+            //IsLoggedIn should be true 
+            if (Convert.ToBoolean(Request.Query["isLoggedIn"]) == false)
+            {
+                Response.StatusCode = 401;
+                return Content("User must be authenticated");
+            }
+
+            return File("/crompton.pdf", "application/pdf");
+        }
+    }
+}
--- ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/Controllers/HomeController.cs
@@ -47,5 +47,40 @@ namespace ControllersExample.Controllers
             };
             return new JsonResult(person);
         }
+
+        // if the file is in wwwroot folder we can use VirtualFileResult
+        [Route("file-download")]
+        public VirtualFileResult FileDownload()
+        {
+            //return new VirtualFileResult("/crompton.pdf", "application/pdf");
+            //In controllers instead of writing VirtualFileResult we can use File as a shortcut 
+            return File("/crompton.pdf", "application/pdf");
+        }
+
+        // if the file is in other folder we can use PhysicalFileResult
+        [Route("file-download2")]
+        public PhysicalFileResult FileDownload2()
+        {
+            return new PhysicalFileResult(@"C:\Users\rahul\OneDrive\Documents\GitHub\ASP.NET\ControllersExample\rahul\crompton.pdf", "application/pdf");
+        }
+
+        // FileContentResult is used when the file data is already available as a byte[] in memory
+        // (e.g., files from database, generated files, or manually read from disk)
+        [Route("file-download3")]
+        public FileContentResult FileDownload3()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(@"C:\Users\rahul\OneDrive\Documents\GitHub\ASP.NET\ControllersExample\rahul\crompton.pdf");
+            //return new FileContentResult(bytes, "application/pdf");
+            //In controllers instead of writing FileContentResult we can use File as a shortcut
+            return File(bytes, "application/pdf");
+        }
+
+        // IActionResult - its the parent interface for all action result classes
+        [Route("file-download4")]
+        public IActionResult FileDownload4()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(@"C:\Users\rahul\OneDrive\Documents\GitHub\ASP.NET\ControllersExample\rahul\crompton.pdf");
+            return File(bytes, "application/pdf");
+        }
     }
 }
