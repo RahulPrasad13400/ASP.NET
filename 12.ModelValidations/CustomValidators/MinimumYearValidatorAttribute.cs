0a8b57bc52c73159e3a26b1e6f8b7b43bc85232d
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-02-23 01:44:23 +0530

    custom validation

---
+++ 12.ModelValidations/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+// CREATING CUSTOM VALIDATORS 
+
+namespace _12.ModelValidations.CustomValidators
+{
+    public class MinimumYearValidatorAttribute : ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefaultErrorMessage { get; set; } = "Year should not be less than {0]";
+
+        // parameterless constructor 
+        public MinimumYearValidatorAttribute()
+        { 
+        }
+
+        // parameterized constructor
+        public MinimumYearValidatorAttribute(int minimumYear)
+        {
+            MinimumYear = minimumYear;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                DateTime date = (DateTime)value;
+                //if(date.Year <= 2000)
+                if (date.Year <= MinimumYear)
+                {
+                    //return new ValidationResult("Minimum year allowed is 2000");
+                    // If the error message is been given the model class we can access it directly by using ErrorMessage
+                    //return new ValidationResult(ErrorMessage);
+                    return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage, MinimumYear));
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            // RETURN NULL MEANS NO VALIDATION
+
+            return null;
+        }
+    }
+}
--- 12.ModelValidations/Models/Person.cs
+++ 12.ModelValidations/Models/Person.cs
@@ -1,16 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using _12.ModelValidations.CustomValidators;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace _12.ModelValidations.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "{0} cannot be empty or null")]
+        // Index number 0 represents the Person Name 
+        // If name is defined in the display it takes up that name
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
         public string? Name { get; set; }
         [EmailAddress]
+        [Required]
         public string? Email { get; set; }
+        [ValidateNever]
         public string? Phone { get; set; }
         public string? Password { get; set; }
+        // It compares the value with the password
+        [Compare("Password")]
         public string? ConfirmPassword { get; set; }
+        // Index number 0 represents the Price
+        // Index number 1 represents the minimum in the range
+        // Index number 2 represents the maximum in the range
+        [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}")]
         public double? Price { get; set; }
+        //[MinimumYearValidatorAttribute]
+        [MinimumYearValidatorAttribute(ErrorMessage ="Date of birth should not be newer than Jan 01, 2000")]
+        public DateTime? DateOfBirth { get; set; }
     }
 }
