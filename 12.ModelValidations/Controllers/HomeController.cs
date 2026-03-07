ca4005cffba76bda7ec8ce4c0fcb11b9c7472595
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-03-07 20:38:30 +0530

    custom model binders

--- 12.ModelValidations/Controllers/HomeController.cs
+++ 12.ModelValidations/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using _12.ModelValidations.Models;
+﻿using _12.ModelValidations.CustomModelBinders;
+using _12.ModelValidations.Models;
 using Microsoft.AspNetCore.Mvc;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -8,7 +9,11 @@ namespace _12.ModelValidations.Controllers
     {
         [HttpPost("register")]
         //public IActionResult Index([Bind("PersonName", "Email")]_12.ModelValidations.Models.Person person)
-        public IActionResult Index([Bind(nameof(Person.Name), nameof(Person.Email))] _12.ModelValidations.Models.Person person)
+        //public IActionResult Index([Bind(nameof(Person.Name), nameof(Person.Email))] _12.ModelValidations.Models.Person person)
+        
+                                    
+                                 // Custom Model Binder
+        public IActionResult Index([ModelBinder(BinderType = typeof(PersonModelBinder))]_12.ModelValidations.Models.Person person)
         {
             if (!ModelState.IsValid)
             {
---
+++ 12.ModelValidations/CustomModelBinders/PersonModelBinder.cs
@@ -0,0 +1,30 @@
+﻿using _12.ModelValidations.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace _12.ModelValidations.CustomModelBinders
+{
+    public class PersonModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            Person person = new Person();
+
+            // Getting the First Name and Last Name
+            if(bindingContext.ValueProvider.GetValue("FirstName").Length > 0)
+            {
+                person.Name = bindingContext.ValueProvider.GetValue("FirstName").FirstValue;
+            }
+
+            if(bindingContext.ValueProvider.GetValue("LastName").Length > 0)
+            {
+                person.Name += " " + bindingContext.ValueProvider.GetValue("LastName").FirstValue;
+            }
+
+            // Also need to bind all the other values otherwise it will all become null
+            // Need to bind values like email, password, and other values ...
+
+            bindingContext.Result = ModelBindingResult.Success(person);
+            return Task.CompletedTask;
+        }
+    }
+}
