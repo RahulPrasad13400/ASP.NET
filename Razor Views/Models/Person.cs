4f5f49bcef20996607bd4d6ff622a19806996830
Rahul Prasad <rahulprasad1399@gmail.com>
2026-03-14 11:08:21 +0530

    if switch foreach

--- Razor Views/Models/Person.cs
+++ Razor Views/Models/Person.cs
@@ -3,6 +3,14 @@
     public class Person
     {
         public string? Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public List<string>? FavouriteMovies { get; set; }
+    }
+    public enum Gender
+    {
+        Male, 
+        Female,
+        Other
     }
 }
